f3be45381240afe90375fcd2a98bb67a1f0f5786
Matthew Edge <mattedgeis@gmail.com>
2016-01-21 19:47:32 -0500

    BaseTest added for convenient ScalaTest inheritance and helper methods.

---
+++ wfa-backend/src/main/scala/edge/wfa/Main.scala
@@ -0,0 +1,8 @@
+package edge.wfa
+
+/**
+  * Created by dev on 1/21/16.
+  */
+object Main extends App {
+
+}
---
+++ wfa-backend/src/test/scala/edge/wfa/BaseTest.scala
@@ -0,0 +1,10 @@
+package edge.wfa
+
+import org.scalatest.{Matchers, FlatSpec}
+
+/**
+  * Base Scala Trait to provide common imports and helper methods
+  *
+  * Created by dev on 1/21/16.
+  */
+trait BaseTest extends FlatSpec with Matchers
